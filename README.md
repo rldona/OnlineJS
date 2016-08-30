0a5a42446ab852002c952a5b109f0158b8c7518a
rldona <rldona@gmail.com>
2016-08-30 18:06:11 +0200

    create demo and pwa refactor

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
-Online
-======
+OnlineJS
+========
 
 Small library that graphically displays the status of your Internet connection (wifi or 4G).
 
+## Live Demo
 
-## Preview
+Visit [https://onlinejs-library.firebaseapp.com/](https://onlinejs-library.firebaseapp.com/) to see a live demo of OnlineJS.
 
-![alt text](./preview.jpg "Preview")
+![alt text](./demo/img/preview.jpg "Preview")
 
-## Demo
+## Setup
 
+**OnlineJS has no dependencies.** It can be added to any web/mobile/pwa app by including two files:
 
-[ https://onlinejs-library.firebaseapp.com/ ](https://onlinejs-library.firebaseapp.com/)
+```HTML
+<!-- OnlineJS -->
+
+<link rel="stylesheet" href="online.min.css" />
+
+<script src="online.min.js"></script>
+```
+
+That is all !!
+
+## Getting Started with OnlineJS
+
+Turn on or off your connection to your network or wifi to check their working demo.
---
+++ src/js/online.js
@@ -0,0 +1,99 @@
+/*! online.js 0.0.1 */
+var Online = (function() {
+
+  'use strict';
+
+  // Private
+  var options = {
+    activate: false,
+    status: navigator.onLine ? true : false,
+    position: 'top'
+  };
+
+  function create() {
+    var alertDOM = document.createElement('div');
+    var backdrop = document.createElement('div');
+
+    alertDOM.classList.add('online-component');
+    alertDOM.classList.add(options.position);
+    backdrop.classList.add('backdrop');
+
+    alertDOM.innerHTML = '<p>Connection lost. Reconnecting...</p><i class="loader"></i>';
+
+    document.body.insertBefore(alertDOM,document.body.childNodes[0]);
+    document.body.insertBefore(backdrop,document.body.childNodes[0]);
+  }
+
+  function remove() {
+    var body = document.querySelector('body');
+    var alert = document.querySelector('.online-component');
+    var backdrop = document.querySelector('.backdrop');
+
+    body.removeChild(alert);
+    body.removeChild(backdrop);
+  }
+
+  function activate() {
+    var alert, backdrop;
+
+    if (!options.status) {
+      create();
+
+      alert = document.querySelector('.online-component');
+      backdrop = document.querySelector('.backdrop');
+
+      backdrop.classList.add('visible');
+      alert.classList.add('visible');
+
+      options.activate = true;
+    }
+
+    if (options.activate && options.status) {
+
+      alert = document.querySelector('.online-component');
+      backdrop = document.querySelector('.backdrop');
+
+      // reconnect
+      alert.classList.add('reconnect');
+      alert.innerHTML = '<p class="connected">Your device is connected to the internet</p>';
+
+      setTimeout(function() {
+        backdrop.classList.remove('visible');
+        alert.classList.remove('visible');
+
+        options.activate = false;
+      }, 1500);
+
+      setTimeout(function() {
+        alert.classList.remove('reconnect');
+        remove();
+      }, 1600);
+    }
+  }
+
+  function updateStatus() {
+    options.status = navigator.onLine ? true : false;
+
+    activate();
+  }
+
+  var init = (function init() {
+    // network listeners
+    window.addEventListener('online',  updateStatus);
+    window.addEventListener('offline', updateStatus);
+
+    // init alert
+    activate(options.status);
+  })();
+
+  // Public
+
+  function getStatus() {
+    return options.status;
+  }
+
+  return {
+    getStatus: getStatus
+  };
+
+})();
